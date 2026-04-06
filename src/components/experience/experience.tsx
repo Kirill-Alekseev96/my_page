6f303c18e2cf0a87bd1cff61e0eab57f8a651dc3
Kirill <kirill_alekseev-96@mail.ru>
2026-04-06 23:23:13 +0300

    '--'

--- src/components/experience/experience.tsx
+++ src/components/experience/experience.tsx
@@ -1,12 +1,18 @@
 export default function Experience () {
     return(
         <section className="experience">
-            <div>
-                <h2>Опыт</h2>
-                <span>год</span>
-                <button>+</button>
+            <h2 className="experience__heading heading">Опыт</h2>
+            <div className="experience__container">
+                <span className="">Должность</span>
+                <div>
+                    <span className="experience__year">год</span>
+                    <button className="experience__button">+</button>
+                </div>
             </div>
-            <p></p>
+
+            <div className="">
+                
+            </div>
         </section>
     )
 }
