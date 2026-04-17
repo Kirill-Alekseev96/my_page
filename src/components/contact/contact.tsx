4ccbab1553faa8e0bbf01ddc47423142c13d0db3
Kirill <kirill_alekseev-96@mail.ru>
2026-04-17 17:40:42 +0300

    com_6

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,15 +1,51 @@
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 export default function Contact() {
+    const YOUR_PUBLIC_KEY:string = 'Wy6_ryLnjhWXKOZ0T';
+    const YOUR_SERVICE_ID:string = 'service_ip7eo26';
+    const YOUR_TEMPLATE_ID: string = 'template_jgk5mvn';
+
+    const form = useRef<HTMLFormElement>(null);
+
+    const sendEmail = (event:React.SyntheticEvent) => {
+    event.preventDefault();
+
+    // Устанавливаем текущую дату
+        const now = new Date();
+        const formattedTime = now.toLocaleString('ru-RU');
+        form.current!.time.value = formattedTime;
+
+    emailjs
+        .sendForm(YOUR_SERVICE_ID, YOUR_TEMPLATE_ID, form.current!, {
+            publicKey: YOUR_PUBLIC_KEY,
+        })
+        .then(
+            () => {
+            console.log('SUCCESS!');
+            form.current!.reset();
+            },
+            (error) => {
+            console.log('FAILED...', error.text);
+            },
+        );
+    };
+
     return (
         <section className="contact">
             <h2 className="contact__heading heading">qw</h2>
             <p className="contact__description">qw</p>
-            <form className="contact__form" action="">
-                <div className="contact__fields">
-                    <input className="contact__input" type="text" />
-                    <input className="contact__input" type="email" />
-                    <textarea className="contact__textarea" name="" id=""></textarea>
-                    <button className="contact__submit" type="submit">Отправить</button>
-                </div>
+            <form id="contact-form" className="contact__form" ref={form} onSubmit={sendEmail}>
+                <input type="hidden" name="time" value="Mar 10 2025 08:46"/>
+                <label>Имя</label>
+                <input type="text" name="name" required/>
+                <label>Почта</label>
+                <input type="email" name="email" required/>
+                <label>Заголовок</label>
+                <input type="text" name="title" required/>
+                <label>Сообщение</label>
+                <textarea name="message" required></textarea>
+                <input type="submit" value="Отправить"/>
             </form>
             <a className="contact__phone" href="tel:+79506874336">+7(950)-687-43-36</a>
         </section>
--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -3,33 +3,62 @@ import Contact from "../contact/contact";
 
 export default function Home () {
 
-    // const [currentText, setCurrentText] = useState('');
-    // const [count, setCount] = useState(0);
-    // const text = 'Разработчик';
+    const [isOpenContact, setIsOpenContact] = useState(false);
+    const [currentText, setCurrentText] = useState('');
+    const text = 'Разработчик';
 
-    // useEffect (() => {
 
-    //     const interval = setInterval(() => {
-    //         if(count < text.length) {
-    //             setCurrentText(prev => prev + text[count]);
-    //             setCount(prev => prev + 1); // prev - это актуальное значение
+    useEffect(() => {
+        let count = 0;
+        let timeoutId;
+        let isMounted: boolean = true; // флаг монтирования
 
-    //         }else{
-    //             clearInterval(interval);
-    //             setTimeout(() => {
-    //                 setCurrentText('');
-    //                 setCount(0);
-    //             }, 2000);
-        
-    //         }
-    //     }, 120)
+        const animate = () => {
+            console.log(count);
+            console.log(currentText);
+            if (!isMounted) return;
+            
+            if (count < text.length) {
+                setCurrentText(prev => prev + text[count]);
+                console.log('Adding:', text[count], 'Result:', currentText);
+                count = count + 1;
+                timeoutId = setTimeout(animate, 120);
+            } else {
+                timeoutId = setTimeout(() => {
+                    if (!isMounted) return;
+                    setCurrentText('');
+                    count = 0;
+                    animate();
+                }, 2000);
+            }   
+        }
 
-    //     return () => clearInterval(interval); 
-    // },[count]);
+        setCurrentText('');
+        count = 0;
+        animate();
+
+        return () => {
+            isMounted = false;
+            clearTimeout(timeoutId);
+        }
+
+    },[]);
+
+
+    
+
+
+    const handleOpenContact = () => {
+        setIsOpenContact(!isOpenContact);
+    }
+
+    const handleCloseContact = () => {
+        setIsOpenContact(!isOpenContact);
+    }
 
     return (
         <section className="home">
-            {/* <div className="home__content">
+            <div className="home__content">
                 <span className="home__greeting">Привет</span>
                 <span className="home__text">Меня зовут Кирилл Алексеев</span>
                 <h1 className="home__heading heading">Я Frontend {currentText}</h1>
@@ -39,13 +68,18 @@ export default function Home () {
                     Постоянно учусь новому и слежу за трендами,чтобы делать веб лучше и быстрее.</p>
                 <div className="home__actions">
                     <button className="home__button--download button">cкачать</button>
-                    <button className="home__button--contact button">написать</button>
+                    <button onClick = {handleOpenContact} className="home__button--contact button">написать</button>
                 </div>
             </div>
             <div className="home__inner">
                 <img className="home__image" src='/images/IMG_4786-Photoroom.png' alt="zxcc"/>
-            </div> */}
-             <Contact/>
+            </div>
+
+            {isOpenContact && 
+                        <>
+                            <div className="slider-backdrop" onClick={handleCloseContact} />
+                            <Contact/>
+                        </>}
         </section>
     )
 } 
